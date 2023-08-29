b10c245a0c1b962f12b33b6423f29c0c56d141c8
libia <libia.roman@ucb.edu.bo>
2023-08-29 14:33:54 +0200

    formato

--- src/presenter.js
+++ src/presenter.js
@@ -20,11 +20,11 @@ form.addEventListener("submit", (event) => {
   let totalDescuento = multiplicar(porcentajeDescuento, total).toFixed(2);
 
   let imp = impuesto(estado.value);
-  let totalImpuesto =  multiplicar(imp,total).toFixed(2);
+  let totalImpuesto =  multiplicar(imp,total-totalDescuento).toFixed(2);
   let totalPrecio =  calcularPrecioTotal(total, totalDescuento, totalImpuesto);
 
-  precioNeto.innerHTML = "<p>Precio neto: (" + firstNumber + " * %" + secondNumber + ") = $" + total + "</p>";
-  desc.innerHTML = "<p>Descuento: %" + multiplicar(porcentajeDescuento,100).toFixed(2) + " = $" + totalDescuento + "</p>";
-  impuestoDiv.innerHTML = "<p>Impuesto para "+estado.value+" es %"+ multiplicar(imp,100).toFixed(2) +" = $" +totalImpuesto+"</p>";
-  TOTALDiv.innerHTML = "<p>PRECIO total "+ totalPrecio +"</p>";
+  precioNeto.innerHTML = "<p>Precio neto (" + firstNumber + "*$" + secondNumber + "): $" + total + "</p>";
+  desc.innerHTML = "<p>Descuento (%" + multiplicar(porcentajeDescuento,100).toFixed(2) + "): $" + totalDescuento + "</p>";
+  impuestoDiv.innerHTML = "<p>Impuesto para "+estado.value+"(%"+ multiplicar(imp,100).toFixed(2) +"): $" +totalImpuesto+"</p>";
+  TOTALDiv.innerHTML = "<p>Precio total (desceunto e impuesto): $"+ totalPrecio +"</p>";
 });
