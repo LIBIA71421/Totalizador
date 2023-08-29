4fb2eac700a6ce993a6f067438e65668587c137b
libia <libia.roman@ucb.edu.bo>
2023-08-29 14:07:02 +0200

    Calcula el 7% de descuento

--- src/sumador.js
+++ src/sumador.js
@@ -6,13 +6,17 @@ function multiplicar(a, b) {
 }
 function descuento(total) {
   let descuento = 0;
-  if (total >= 1000 && total <= 3000)
+  if (total >= 1000 && total < 3000)
   {
     descuento = 0.03;
   }
-  else if (total >= 3000 && total <= 7000)
+  else if (total >= 3000 && total < 7000)
   {
     descuento = 0.05;
+  }
+  else if (total >= 7000 && total < 10000)
+  {
+    descuento = 0.07;
   }
 
   return descuento;
